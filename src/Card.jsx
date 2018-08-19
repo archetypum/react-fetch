a8e57c912c78d930288cf24efdad22ce15f4729f
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-08-19 22:28:26 +0200

    robots.js and Card component added.

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// props deconstruction AHEAD:
+const Card = ({name, email, id}) => { 
+
+    return (
+        <div className="bg-light-green dib br3 pa3 ma2 grow b2 shadow-5" >
+            <img alt='robots' src={`https://www.robohash.org/${id}?200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
