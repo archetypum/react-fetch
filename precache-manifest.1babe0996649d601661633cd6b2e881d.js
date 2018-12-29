00015be488446c091165e753ba4affaeeeceb9ad
hakki riza kucuk <hakkirizakucuk@yandex.com>
2018-12-29 18:41:12 +0100

    Updates

---
+++ precache-manifest.1babe0996649d601661633cd6b2e881d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ab85a9bf9b512918237",
+    "url": "/react-fetch/static/css/main.39e3b317.chunk.css"
+  },
+  {
+    "revision": "3ab85a9bf9b512918237",
+    "url": "/react-fetch/static/js/main.3ab85a9b.chunk.js"
+  },
+  {
+    "revision": "26a96394ba123431482e",
+    "url": "/react-fetch/static/css/1.6b114bbb.chunk.css"
+  },
+  {
+    "revision": "26a96394ba123431482e",
+    "url": "/react-fetch/static/js/1.26a96394.chunk.js"
+  },
+  {
+    "revision": "cfea8fef0bc0a384591d",
+    "url": "/react-fetch/static/js/runtime~main.cfea8fef.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-fetch/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "15449026252b4cd6e11915ffed011c71",
+    "url": "/react-fetch/index.html"
+  }
+];
